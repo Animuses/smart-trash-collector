3f7a4ee6563ce4a1a3e3f9d21485c83b75f5fd0c
Animuses <animuses@hotmail.com>
2021-07-04 02:07:37 +0800

    added servo function,need test

--- CAR.py
+++ CAR.py
@@ -1,20 +1,43 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 #通用调用库 By Animuses
+#
 import RPi.GPIO as GPIO
 import time
 
+
+#定义变量设定引脚接口与pwm
+LIN1 = 20
+LIN2 = 21
+LPWM = 16
+RIN1 = 19
+RIN2 = 26
+RPWM = 13
+BUZZER = 8
+H_BASEH = 23
+H_BASEV = 11
+H_JOINT = 9
+H_HANDR = 10
+H_HAND = 25
+pwm_LPWM = GPIO.PWM(LPWM,2000)
+pwm_RPWM = GPIO.PWM(RPWM,2000)
+pwm_LPWM.start(0)
+pwm_RPWM.start(0)
+pwm_H_BASEH = GPIO.PWM(H_BASEH,50)
+pwm_H_BASEV = GPIO.PWM(H_BASEV,50)
+pwm_H_JOINT = GPIO.PWM(H_JOINT,50)
+pwm_H_HANDR = GPIO.PWM(H_HANDR,50)
+pwm_H_HAND = GPIO.PWM(H_HAND,50)
+pwm_H_BASEH.start(7.5)
+pwm_H_BASEV.start(7.5)
+pwm_H_JOINT.start(7.5)
+pwm_H_HANDR.start(7.5)
+pwm_H_HAND.start(7.5)
 #初始化，设定引脚状态
+#L为左轮，R为右轮，PWM为占空比，调速用，H为机械臂
 def  init():
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-    global LIN1,LIN2,RIN1,RIN2,LPWM,RPWM,BUZZER,pwm_LPWM,pwm_RPWM
-    LIN1 = 20
-    LIN2 = 21
-    LPWM = 16
-    RIN1 = 19
-    RIN2 = 26
-    RPWM = 13
-    BUZZER = 8
+    #初始化GPIO引脚和初始化PWM并复位
     GPIO.setup(LPWM,GPIO.OUT)
     GPIO.setup(LIN1,GPIO.OUT)
     GPIO.setup(LIN2,GPIO.OUT)
@@ -22,18 +45,35 @@ def  init():
     GPIO.setup(RIN1,GPIO.OUT)
     GPIO.setup(RIN2,GPIO.OUT)
     GPIO.setup(BUZZER,GPIO.OUT)
-    pwm_LPWM = GPIO.PWM(LPWM,2000)
-    pwm_RPWM = GPIO.PWM(RPWM,2000)
-    pwm_LPWM.start(0)
-    pwm_RPWM.start(0)
+    GPIO.setup(H_BASEH,GPIO.OUT)
+    GPIO.setup(H_BASEV,GPIO.OUT)
+    GPIO.setup(H_JOINT,GPIO.OUT)
+    GPIO.setup(H_HANDR,GPIO.OUT)
+    GPIO.setup(H_HAND,GPIO.OUT)
     beep()
     #初始化完成后蜂鸣器响一声
 
+#初始化结束
+
+#############
+#通用部件部分#
+#############
+
+#蜂鸣器响一声
 def beep():
     GPIO.output(BUZZER, False)
     time.sleep(0.1)
     GPIO.output(BUZZER, True)
-    time.sleep(0.001)
+    time.sleep(0.005)
+
+#关机
+def shutdown():
+    GPIO.cleanup()
+
+
+##############
+##小车运动部分#
+##############
 
 #停车
 def stop():
@@ -80,17 +120,39 @@ def right_spin(speed):
 
 #行进中左转*测试中
 def left_running(speed):
-    pwm_LPWM.ChangeDutyCycle(80-speed)
-    pwm_RPWM.ChangeDutyCycle(80+speed)
+    pwm_LPWM.ChangeDutyCycle(speed)
+    pwm_RPWM.ChangeDutyCycle(speed)
 
 #行进中右转*测试中
 def right_running(speed):
-    pwm_LPWM.ChangeDutyCycle(80+speed)
-    pwm_RPWM.ChangeDutyCycle(80-speed)
-#关机
-def shutdown():
-    GPIO.cleanup()
+    pwm_LPWM.ChangeDutyCycle(speed)
+    pwm_RPWM.ChangeDutyCycle(speed)
+
+
+##############
+##舵机控制部分#
+##############
+
+#角度转换为PWM，范围：0-270
+def Servo_angle(angle):
+    duty = (angle/270+0.25)*10
+    return(duty)
+
+#舵机旋转至特定角度
+def Servo_rotate(pwm_servo,target):
+    pwm_servo.ChangeDutyCycle(target)
+
+#舵机正转（仅适用于遥控）
+def Servo_pos(pwm_servo,angle_now):
+    for angle_now in range(2.5,12.5):
+        angle_now=angle_now+0.1
+        Servo_rotate(pwm_servo,angle_now)
+
+#舵机反转（仅适用于遥控）
+def Servo_neg(pwm_servo,angle_now):
+    for angle_now in range(2.5,12.5):
+        angle_now=angle_now-0.1
+        Servo_rotate(pwm_servo,angle_now)
+
 
 #结束
-
-
--- remote.py
+++ remote.py
@@ -1,5 +1,7 @@
+#/usr/bin/python3
 #遥控程序 By Animuses
 import CAR
+from CAR import *
 import time
 import pygame
 from pygame.locals import *
@@ -9,21 +11,58 @@ CAR.init()
 pygame.init()
 size = width, hight = 100,100
 screen = pygame.display.set_mode(size)
+speed = 80
+turn_speed = 80
+HBHangle_now = 7.5
+HBVangle_now = 7.5
+HBJangle_now = 7.5
+HHRangle_now = 7.5
+HHangle_now = 7.5
 while True:
     for event in pygame.event.get():
         if event.type == KEYDOWN:
-            if event.key == K_UP:
-                CAR.forward(80)
+            if event.key == K_w:
+                CAR.forward(speed)
                 print('Forward')
-            if event.key == K_DOWN:
-                CAR.backward(80)
+            if event.key == K_s:
+                CAR.backward(speed)
                 print('Backward')
-            if event.key == K_LEFT:
-                CAR.left_spin(80)
+            if event.key == K_a:
+                CAR.left_spin(speed)
                 print('Left')
-            if event.key == K_RIGHT:
-                CAR.right_spin(80)
+            if event.key == K_d:
+                CAR.right_spin(speed)
                 print('Right')
+            if event.key == K_UP:
+                CAR.Servo_pos(pwm_H_BASEV,HBVangle_now)
+                print('HBUP')
+            if event.key == K_DOWN:
+                CAR.Servo_neg(pwm_H_BASEV,HBVangle_now)
+                print('HBDOWN')
+            if event.key == K_LEFT:
+                CAR.Servo_pos(pwm_H_BASEH,HBVangle_now)
+                print('HBLEFT')
+            if event.key == K_RIGHT:
+                CAR.Servo_neg(pwm_H_BASEH,HBVangle_now)
+                print('HBRIGHT')
+            if event.key == K_r:
+                CAR.Servo_pos(pwm_H_JOINT,HBVangle_now)
+                print('HJUP')
+            if event.key == K_f:
+                CAR.Servo_neg(pwm_H_JOINT,HBVangle_now)
+                print('HJDOWN')
+            if event.key == K_g:
+                CAR.Servo_pos(pwm_H_HANDR,HBVangle_now)
+                print('HHLEFT')
+            if event.key == K_h:
+                CAR.Servo_neg(pwm_H_HANDR,HBVangle_now)
+                print('HHRIGHT')
+            if event.key == K_v:
+                CAR.Servo_pos(pwm_H_HAND,HBVangle_now)
+                print('HHOPEN')
+            if event.key == K_b:
+                CAR.Servo_neg(pwm_H_HAND,HBVangle_now)
+                print('HHCLOSE')
             if event.key == K_ESCAPE:
                 print('Quit!')
                 CAR.beep()
