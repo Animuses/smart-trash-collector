8c6ed973b5aa6fd9bfd892794f3efc734eec6a80
Animuses <animuses@hotmail.com>
2021-07-20 21:31:54 +0800

    Major BUG Fixed, Block SERVO Turn To STM32

--- CAR.py
+++ CAR.py
@@ -6,6 +6,8 @@ import time
 
 
 #定义变量设定引脚接口与pwm
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 LIN1 = 20
 LIN2 = 21
 LPWM = 16
@@ -13,67 +15,74 @@ RIN1 = 19
 RIN2 = 26
 RPWM = 13
 BUZZER = 8
-H_BASEH = 23
-H_BASEV = 11
-H_JOINT = 9
-H_HANDR = 10
-H_HAND = 25
+SERVO1 = 23
+SERVO2 = 11
+SERVO3 = 9
+SERVO4 = 10
+SERVO5 = 25
+SERVO6 = 2
+#初始化GPIO引脚和初始化PWM并复位
+GPIO.setup(LPWM,GPIO.OUT)
+GPIO.setup(LIN1,GPIO.OUT)
+GPIO.setup(LIN2,GPIO.OUT)
+GPIO.setup(RPWM,GPIO.OUT)
+GPIO.setup(RIN1,GPIO.OUT)
+GPIO.setup(RIN2,GPIO.OUT)
+GPIO.setup(BUZZER,GPIO.OUT)
+GPIO.setup(SERVO1,GPIO.OUT)
+GPIO.setup(SERVO2,GPIO.OUT)
+GPIO.setup(SERVO3,GPIO.OUT)
+GPIO.setup(SERVO4,GPIO.OUT)
+GPIO.setup(SERVO5,GPIO.OUT)
+GPIO.setup(SERVO6,GPIO.OUT)
 pwm_LPWM = GPIO.PWM(LPWM,2000)
 pwm_RPWM = GPIO.PWM(RPWM,2000)
 pwm_LPWM.start(0)
 pwm_RPWM.start(0)
-pwm_H_BASEH = GPIO.PWM(H_BASEH,50)
-pwm_H_BASEV = GPIO.PWM(H_BASEV,50)
-pwm_H_JOINT = GPIO.PWM(H_JOINT,50)
-pwm_H_HANDR = GPIO.PWM(H_HANDR,50)
-pwm_H_HAND = GPIO.PWM(H_HAND,50)
-pwm_H_BASEH.start(7.5)
-pwm_H_BASEV.start(7.5)
-pwm_H_JOINT.start(7.5)
-pwm_H_HANDR.start(7.5)
-pwm_H_HAND.start(7.5)
+pwm_SERVO1 = GPIO.PWM(SERVO1,50)
+pwm_SERVO2 = GPIO.PWM(SERVO2,50)
+pwm_SERVO3 = GPIO.PWM(SERVO3,50)
+pwm_SERVO4 = GPIO.PWM(SERVO4,50)
+pwm_SERVO5 = GPIO.PWM(SERVO5,50)
+pwm_SERVO6 = GPIO.PWM(SERVO6,50)
+pwm_SERVO1.start(7.5)
+pwm_SERVO2.start(7.5)
+pwm_SERVO3.start(7.5)
+pwm_SERVO4.start(7.5)
+pwm_SERVO5.start(7.5)
+pwm_SERVO6.start(7.5)
+
 #初始化，设定引脚状态
-#L为左轮，R为右轮，PWM为占空比，调速用，H为机械臂
+#L为左轮，R为右轮，PWM为占空比，调速用，SERVO为舵机接口
 def  init():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    #初始化GPIO引脚和初始化PWM并复位
-    GPIO.setup(LPWM,GPIO.OUT)
-    GPIO.setup(LIN1,GPIO.OUT)
-    GPIO.setup(LIN2,GPIO.OUT)
-    GPIO.setup(RPWM,GPIO.OUT)
-    GPIO.setup(RIN1,GPIO.OUT)
-    GPIO.setup(RIN2,GPIO.OUT)
-    GPIO.setup(BUZZER,GPIO.OUT)
-    GPIO.setup(H_BASEH,GPIO.OUT)
-    GPIO.setup(H_BASEV,GPIO.OUT)
-    GPIO.setup(H_JOINT,GPIO.OUT)
-    GPIO.setup(H_HANDR,GPIO.OUT)
-    GPIO.setup(H_HAND,GPIO.OUT)
-    beep()
-    #初始化完成后蜂鸣器响一声
+    print("Initialize success")
+    beep(0.05)
+    beep(0.05)
+    #初始化完成后蜂鸣器响两声
 
 #初始化结束
 
-#############
-#通用部件部分#
-#############
+###############
+##通用部件部分##
+###############
 
 #蜂鸣器响一声
-def beep():
+def beep(time):
     GPIO.output(BUZZER, False)
-    time.sleep(0.1)
+    time.sleep(0.05)
     GPIO.output(BUZZER, True)
-    time.sleep(0.005)
+    time.sleep(time)
+    GPIO.output(BUZZER, False)
 
-#关机
+#关机 完成后长响一声
 def shutdown():
+    beep(0.1)
     GPIO.cleanup()
 
 
-##############
-##小车运动部分#
-##############
+###############
+##小车运动部分##
+###############
 
 #停车
 def stop():
@@ -118,21 +127,11 @@ def right_spin(speed):
     GPIO.output(RIN1, False)
     GPIO.output(RIN2, True)
 
-#行进中左转*测试中
-def left_running(speed):
-    pwm_LPWM.ChangeDutyCycle(speed)
-    pwm_RPWM.ChangeDutyCycle(speed)
-
-#行进中右转*测试中
-def right_running(speed):
-    pwm_LPWM.ChangeDutyCycle(speed)
-    pwm_RPWM.ChangeDutyCycle(speed)
-
-
-##############
-##舵机控制部分#
-##############
-
+###############
+##舵机控制部分##
+##目前尚在测试##
+###############
+'''
 #角度转换为PWM，范围：0-270
 def Servo_angle(angle):
     duty = (angle/270+0.25)*10
@@ -153,6 +152,6 @@ def Servo_neg(pwm_servo,angle_now):
     for angle_now in range(2.5,12.5):
         angle_now=angle_now-0.1
         Servo_rotate(pwm_servo,angle_now)
-
+'''
 
 #结束
--- remote.py
+++ remote.py
@@ -13,11 +13,6 @@ size = width, hight = 100,100
 screen = pygame.display.set_mode(size)
 speed = 80
 turn_speed = 80
-HBHangle_now = 7.5
-HBVangle_now = 7.5
-HBJangle_now = 7.5
-HHRangle_now = 7.5
-HHangle_now = 7.5
 while True:
     for event in pygame.event.get():
         if event.type == KEYDOWN:
@@ -33,39 +28,8 @@ while True:
             if event.key == K_d:
                 CAR.right_spin(speed)
                 print('Right')
-            if event.key == K_UP:
-                CAR.Servo_pos(pwm_H_BASEV,HBVangle_now)
-                print('HBUP')
-            if event.key == K_DOWN:
-                CAR.Servo_neg(pwm_H_BASEV,HBVangle_now)
-                print('HBDOWN')
-            if event.key == K_LEFT:
-                CAR.Servo_pos(pwm_H_BASEH,HBVangle_now)
-                print('HBLEFT')
-            if event.key == K_RIGHT:
-                CAR.Servo_neg(pwm_H_BASEH,HBVangle_now)
-                print('HBRIGHT')
-            if event.key == K_r:
-                CAR.Servo_pos(pwm_H_JOINT,HBVangle_now)
-                print('HJUP')
-            if event.key == K_f:
-                CAR.Servo_neg(pwm_H_JOINT,HBVangle_now)
-                print('HJDOWN')
-            if event.key == K_g:
-                CAR.Servo_pos(pwm_H_HANDR,HBVangle_now)
-                print('HHLEFT')
-            if event.key == K_h:
-                CAR.Servo_neg(pwm_H_HANDR,HBVangle_now)
-                print('HHRIGHT')
-            if event.key == K_v:
-                CAR.Servo_pos(pwm_H_HAND,HBVangle_now)
-                print('HHOPEN')
-            if event.key == K_b:
-                CAR.Servo_neg(pwm_H_HAND,HBVangle_now)
-                print('HHCLOSE')
             if event.key == K_ESCAPE:
                 print('Quit!')
-                CAR.beep()
                 CAR.shutdown()
                 pygame.quit()
                 os._exit(0)
